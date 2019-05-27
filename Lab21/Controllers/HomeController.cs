5d7caa4197e39db809676ab96d940d6849a210c3
Nick <nick.portell12@gmail.com>
2019-05-27 13:35:01 -0400

    First commit for lab 22

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Lab22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Summary(string firstName, string lastName, string email, string phone, string password)
+        {
+            
+            
+
+            User user = new User(ToTitle(firstName),ToTitle(lastName),email,phone,password);
+            return View(user);
+        }
+
+        public string ToTitle(string word)
+        {
+            string newWord = "";
+
+            for(int i = 0; i < word.Length ;i++)
+            {
+                if (i == 0)
+                {
+                    newWord += word[i].ToString().ToUpper();
+                }
+                else
+                {
+                    newWord += word[i].ToString().ToLower();
+                }
+            }
+            return newWord;
+        }
+        
+
+
+
+
+    }
+}
---
+++ Lab21/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab22.Models
+{
+    public class User
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private string _phone;
+        private string _password;
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string password { get; set; }
+
+        public User(string FirstName, string LastName, string Email, string Phone, string Password)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            email = Email;
+            phone = Phone;
+            password = Password;
+        }
+
+    }
+}
