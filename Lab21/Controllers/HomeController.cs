41fbd808adfc49d68c25d9dfe8ef9468c0ccbe2a
Nick <nick.portell12@gmail.com>
2019-05-30 20:42:38 -0400

    Commit for lab 24

--- Lab21/Controllers/HomeController.cs
+++ Lab21/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Lab22.Models;
+﻿using Lab21.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,32 +14,14 @@ namespace Lab21.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult Registration()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Summary(string firstName, string lastName, string email, string phone, string password)
+        public ActionResult Summary(User user)
         {
-            
-            
-
-            User user = new User(ToTitle(firstName),ToTitle(lastName),email,phone,password);
             return View(user);
         }
 
---
+++ Lab21/Controllers/ShopController.cs
@@ -0,0 +1,84 @@
+﻿using Lab21.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class ShopController : Controller
+    {
+        ShopDBEntities ORM = new ShopDBEntities();
+
+        // GET: Item
+        public ActionResult Index()
+        {
+            ViewBag.ItemsList = ORM.Items.ToList();
+            return View();
+        }
+
+        public ActionResult UpdateItem(int itemID)
+        {
+            Item found = ORM.Items.Find(itemID);
+
+            //if(ModelState.IsValid)
+            //{
+
+            //}
+
+            return View(found);
+        }
+
+        public ActionResult SaveChanges(Item updateItem)
+        {
+            Item originalItem = ORM.Items.Find(updateItem.Id);
+
+            if (originalItem != null && ModelState.IsValid)
+            {
+                originalItem.Name = updateItem.Name;
+                originalItem.Description = updateItem.Description;
+                originalItem.Quantity = updateItem.Quantity;
+                originalItem.Price = updateItem.Price;
+
+                ORM.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Something did not go right. Try again.";
+                return RedirectToAction("UpdateItem", updateItem.Id);
+            }
+        }
+
+        public ActionResult DeleteItem(int itemID)
+        {
+            Item found = ORM.Items.Find(itemID);
+            ORM.Items.Remove(found);
+            ORM.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SaveNewItem(Item newItem)
+        {
+            if (ModelState.IsValid)
+            {
+                ORM.Items.Add(newItem);
+                ORM.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Something didn't go right..";
+                return View("ItemForm");
+            }
+        }
+
+        public ActionResult ItemForm()
+        {
+            return View();
+        }
+
+    }
+}
